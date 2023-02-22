a506dc2be35e751d75a6bda8a6f51527fab9d650
abu-kausar <abukausar33@gmail.com>
2023-02-23 02:00:21 +0600

    get and post method for orders collection added

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try{
         const serviceCollection = client.db("carDoctor").collection("services");
+        const orderCollection = client.db("carDoctor").collection("orders");
 
+        //service api
         app.get('/services', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query);
@@ -29,6 +31,25 @@ async function run() {
             const query = {_id: new ObjectId(id)};
             const service = await serviceCollection.findOne(query);
             res.send(service);
+        });
+
+        // order api
+        app.get('/orders', async (req, res) => {
+            let query = {};
+            if(req.query.email){
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        });
+
+        app.post('/orders', async(req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
         });
     }
     finally{
